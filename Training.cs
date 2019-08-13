f3b9304a968133bea59f6649792776d29dfecffd
Nuthan <nuthanknataraju@gmail.com>
2019-08-13 12:15:46 +0530

    new file added

---
+++ Training.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib_TrngMgmt
+{
+   public class Training
+    {
+        public string TrngID { get; set; }
+        public string Title { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
